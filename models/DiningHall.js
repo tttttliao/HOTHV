c5086b98d5037ad8962375f0fed8ed6b6f9daa8a
Yiwei(Tony) Liao <tonyliao0406@foxmail.com>
2018-12-07 01:46:23 -0800

    Comment Styling Pt1

--- models/DiningHall.js
+++ models/DiningHall.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var diningHallSchema = new mongoose.Schema({
     name: String,
     image: String,
+    description: String,
     comments:[
     	{
     		type:mongoose.Schema.Types.ObjectId,
